916e74f7af51b81ebe82997fa7b052f8dbf934be
Akarin9 <9tangyue9@gmail.com>
2019-03-10 19:10:26 +0800

    first

---
+++ src/com/soft1841/jpt/day1/Market.java
@@ -0,0 +1,15 @@
+package com.soft1841.jpt.day1;
+
+/**
+ * 商场抽象类
+ * 2019.3.10
+ *
+ * @author Yue Tang
+ */
+
+public abstract class Market {
+    public String name;
+    public String goods;
+
+    public abstract void shop();
+}

1ab0d32db8c180aea82788a8bbbda6c46a3f5385
Akarin9 <9tangyue9@gmail.com>
2019-04-18 06:37:40 +0800

    爬虫

---
+++ src/com/soft1841/jpt/day27/JSoupDemo.java
@@ -0,0 +1,22 @@
+package com.soft1841.jpt.day27;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class JSoupDemo {
+    public static void main(String[] args) throws Exception{
+        String url = "https://www.jianshu.com/";
+        Connection connection = Jsoup.connect(url);
+        Document document = connection.get();
+        Elements elements = document.getElementsByClass("content");
+        System.out.println(elements.size());
+        for (Element element : elements){
+            Element link = element.child(0);
+            String titleString = link.text();
+            System.out.println(titleString);
+        }
+    }
+}

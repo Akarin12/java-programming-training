c0ffa5fbafd03b3fdbab6be16ed9c2a78741e20e
Akarin9 <9tangyue9@gmail.com>
2019-05-23 06:24:39 +0800

    代理模式

---
+++ src/com/soft1841/jpt/ls/RealSubject.java
@@ -0,0 +1,8 @@
+package com.soft1841.jpt.ls;
+
+public class RealSubject implements Subject{
+    @Override
+    public void buyMac() {
+        System.out.println("买一台MAC");
+    }
+}
---
+++ src/com/soft1841/jpt/ls/Subject.java
@@ -0,0 +1,5 @@
+package com.soft1841.jpt.ls;
+
+public interface Subject {
+    public void buyMac();
+}
